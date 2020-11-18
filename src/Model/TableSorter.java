b4f388f391dc490d704a5e3416fed416f200a24a
Conner <32659799+conner-s@users.noreply.github.com>
2020-11-18 18:36:14 -0500

    Lambda Comparator Choosing

--- src/Model/TableSorter.java
+++ src/Model/TableSorter.java
@@ -21,84 +21,22 @@ public class TableSorter {
     //Gets Comparator based on field number passed
     public Comparator<TableMember> getComparator(int sortField) {
 
-        Comparator<TableMember> returnComp = null;
+        Comparator<TableMember> returnComp;
+        //Comparator for the number field
+        if (sortField == 0) {
+            returnComp = (o1, o2) -> {
+                if (Integer.parseInt(o1.getAttribute(0)) < Integer.parseInt(o2.getAttribute(0))){
+                    return -1;
+                } else if (Integer.parseInt(o1.getAttribute(0)) == Integer.parseInt(o2.getAttribute(0))){
+                    return 0;
+                } else {
+                    return 1;
+                }
+            };
+        //Comparator for everything else, works for weight as well since all weights are 3 digits
+        } else {
+            returnComp = Comparator.comparing(o -> o.getAttribute(sortField));
 
-        switch (sortField) {
-            case 0:
-                returnComp = new Comparator<TableMember>() {
-                    @Override
-                    public int compare(TableMember o1, TableMember o2) {
-                        if (Integer.parseInt(o1.getAttribute(0)) < Integer.parseInt(o2.getAttribute(0))){
-                            return -1;
-                        } else if (Integer.parseInt(o1.getAttribute(0)) == Integer.parseInt(o2.getAttribute(0))){
-                            return 0;
-                        } else {
-                            return 1;
-                        }
-                    }
-                };
-                break;
-
-            //Player Position
-            case 1:
-                returnComp = new Comparator<TableMember>() {
-                    @Override
-                    public int compare(TableMember o1, TableMember o2) {
-                        return o1.getAttribute(1).compareTo(o2.getAttribute(1));
-                    }
-                };
-                break;
-
-            //Player Name
-            case 2:
-                returnComp = new Comparator<TableMember>() {
-                    @Override
-                    public int compare(TableMember o1, TableMember o2) {
-                        return o1.getAttribute(2).compareTo(o2.getAttribute(2));
-                    }
-                };
-                break;
-
-            //Player Height
-            case 3:
-                returnComp = new Comparator<TableMember>() {
-                    @Override
-                    public int compare(TableMember o1, TableMember o2) {
-                        return o1.getAttribute(3).compareTo(o2.getAttribute(3));
-                    }
-                };
-                break;
-
-            //Player Weight
-            case 4:
-                returnComp = new Comparator<TableMember>() {
-                    @Override
-                    public int compare(TableMember o1, TableMember o2) {
-                        return o1.getAttribute(4).compareTo(o2.getAttribute(4));
-                    }
-                };
-                break;
-
-            //Player Hometown
-            //NOTE: Leaving String comparison in due to there being no single/double digit weights and String comparison works
-            case 5:
-                returnComp = new Comparator<TableMember>() {
-                    @Override
-                    public int compare(TableMember o1, TableMember o2) {
-                        return o1.getAttribute(5).compareTo(o2.getAttribute(5));
-                    }
-                };
-                break;
-
-            //Player Highschool
-            case 6:
-                returnComp = new Comparator<TableMember>() {
-                    @Override
-                    public int compare(TableMember o1, TableMember o2) {
-                        return o1.getAttribute(6).compareTo(o2.getAttribute(6));
-                    }
-                };
-                break;
         }
 
         return returnComp;
